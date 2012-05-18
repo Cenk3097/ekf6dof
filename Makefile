7cfff49ac32053cd947a9444a6b0a8c3e19f63a4
Nicolau Werneck <nwerneck@gmail.com>
2012-05-18 00:13:20 -0300

    example from OpenCV's Kalman class

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 run_test: run_test.cc
-	g++ run_test.cc -o run_test
+	g++ run_test.cc -o run_test `pkg-config --libs --cflags opencv`
 
 
--- run_test.cc
+++ run_test.cc
@@ -1,9 +1,14 @@
 #include <iostream>
 
+#include <cv.h>
+
+
 using namespace std;
 
 int main(int argc, char **argv){
   
   cout << "Hello world!";
+
+
   
 }
